ae9f0d6f7e9957c5cbfa20e6c839efe384f783a0
jdy4k <jhosler02@gmail.com>
2025-12-29 00:20:50 -0500

    misc

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -3,6 +3,7 @@
   imports = [ (import ./network.nix) ]
     ++ [ (import ./pipewire.nix) ]
     ++ [ (import ./system.nix) ]
+    ++ [ (import ./fonts.nix) ]
     ++ [ (import ./boot.nix) ]
     ++ [ (import ./xdg.nix) ];
 }
--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -26,10 +26,6 @@
   ];
 
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.supportedLocales = [
-    "en_US.UTF-8/UTF-8"
-    "ja_JP.UTF-8/UTF-8"
-  ];
 
   # Font packages
   fonts.packages = [
