d74105c6d4fabfd11b939fa0d3f2eb05a9478f4a
Jack Hosler <jhosler02@gmail.com>
2026-01-17 18:33:37 -0500

    misc

--- modules/profiles/lang-tools/mpv/default.nix
+++ modules/profiles/lang-tools/mpv/default.nix
@@ -1,13 +1,9 @@
 { pkgs, ... }: 
 {
   programs.mpv = {
-    enable = true;
-    package = pkgs.mpv;
     scripts = with pkgs.mpvScripts; [
-      autosubsync-mpv
-      uosc
+      mpvacious
     ];
   };
-  xdg.configFile."mpv/mpv.conf".source = ./conf/mpv.conf;
   xdg.configFile."mpv/script-opts".source = ./conf/script-opts;
 }
